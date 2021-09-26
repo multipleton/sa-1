06f60ae0b439508c5c7da7cec96f9ee3b1b431b0
Yehor Bublyk <yehor.bk@gmail.com>
2021-09-26 12:03:52 +0300

    basic http-server functionality added

--- server.go
+++ server.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/time", timeHandler)
+	http.ListenAndServe(":8795", nil)
 }
