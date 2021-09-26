dc56116721c19834bdfaa2e6dcff8e26550796f7
lightballer <bokiiartem@gmail.com>
2021-09-26 12:23:27 +0300

    add json format

--- server.go
+++ server.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 )
+
+type model struct {
+	Time string `json:"time"`
+}
 
 func getTimeString() string {
 	return time.Now().Format(time.RFC3339)
@@ -12,7 +17,13 @@ func getTimeString() string {
 
 func timeHandler(w http.ResponseWriter, r *http.Request) {
 	currentTime := getTimeString()
-	fmt.Fprintf(w, currentTime)
+	result := model{Time: currentTime}
+	encoded, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, string(encoded))
 }
 
 func main() {
